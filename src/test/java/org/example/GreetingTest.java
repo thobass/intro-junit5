7f0184eb5adce33825c08467078c17b019f12560
BASSET, Thomas <thomas.basset@cgi.com>
2022-11-17 08:04:41 +0100

    Create test and add before each.

---
+++ src/test/java/org/example/GreetingTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+    private Greeting greeting;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("In before each...");
+        greeting = new Greeting();
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void testHelloWorld() {
+        System.out.println(greeting.helloWorld("Thomas"));
+    }
+}
