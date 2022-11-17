dbc447eb28e8ed7c71f2a1ddd0bab61b2da352e4
BASSET, Thomas <thomas.basset@cgi.com>
2022-11-17 08:22:56 +0100

    Add afterEach, before class, after class

--- src/test/java/org/example/GreetingTest.java
+++ src/test/java/org/example/GreetingTest.java
@@ -1,12 +1,16 @@
 package org.example;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
     private Greeting greeting;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before - I am only called once !!!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -22,5 +26,20 @@ class GreetingTest {
     @Test
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("Thomas"));
+    }
+
+    @Test
+    void testHelloWorld2() {
+        System.out.println(greeting.helloWorld("Sam"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In after each....");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After - I am only called once !!!!");
     }
 }
